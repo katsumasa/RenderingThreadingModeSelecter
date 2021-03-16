80a3af50d72dbb9243c2af707a75d383980705a9
katsumasa <katsumasa@unity3d.com>
2021-03-16 12:18:53 +0900

    Add files via upload

---
+++ Editor/RenderingThreadingModeSelecter.cs
@@ -0,0 +1,207 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+#if UNITY_EDITOR
+using UnityEditor;
+
+namespace Utj
+{
+    /// <summary>    
+    /// RenderingThreadingModeの切り替えをMenuから行う為のClassです。
+    /// "Edit/RenderingThreadingMode" からモードを切り替える事が出来ます。
+    /// 
+    /// RenderingThreadingModeは PlayerSettings.MTRendering、PlayerSettings.graphicsJobs、PlayerSettings.graphicsJobsModeを組み合わせて指定します。
+    /// 関しては下記のURLを参照して下さい。
+    /// https://docs.unity3d.com/ScriptReference/Rendering.RenderingThreadingMode.html
+    /// https://learn.unity.com/tutorial/optimizing-graphics-in-unity#5c7f8528edbc2a002053b5ad
+    /// </summary>
+    public class RenderingThreadingModeSelector
+    {
+        /// <summary>
+        /// RenderingThreadingMode To Menu Path
+        /// </summary>
+        static Dictionary<UnityEngine.Rendering.RenderingThreadingMode, string> mRenderingThreadingModePaths = new Dictionary<UnityEngine.Rendering.RenderingThreadingMode, string>()
+        {
+            {UnityEngine.Rendering.RenderingThreadingMode.Direct,"Edit/RenderingThreadingMode/Direct" },
+            {UnityEngine.Rendering.RenderingThreadingMode.MultiThreaded,"Edit/RenderingThreadingMode/MultiThreaded" },
+            {UnityEngine.Rendering.RenderingThreadingMode.LegacyJobified,"Edit/RenderingThreadingMode/LegacyJobified" },
+            {UnityEngine.Rendering.RenderingThreadingMode.NativeGraphicsJobs,"Edit/RenderingThreadingMode/NativeGraphicsJobs" },
+            {UnityEngine.Rendering.RenderingThreadingMode.NativeGraphicsJobsWithoutRenderThread,"Edit/RenderingThreadingMode/NativeGraphicsJobsWithoutRenderThread" },
+        };
+
+
+        /// <summary>
+        /// Unity エディターを読み込んだ時に実行する処理
+        /// </summary>
+        [InitializeOnLoadMethod]
+        static void InitializeOnLoad()
+        {
+            EditorApplication.update += RenderingThreadingModeChecker;
+        }
+
+
+        /// <summary>
+        /// Menuにチェックマークを付ける処理
+        /// </summary>
+        /// <param name="renderingThreadingMode">チェックを付けたい項目</param>
+        static void SetMenu(UnityEngine.Rendering.RenderingThreadingMode renderingThreadingMode)
+        {
+            foreach (var key in mRenderingThreadingModePaths.Keys)
+            {
+                Menu.SetChecked(mRenderingThreadingModePaths[key], key == renderingThreadingMode);
+            }
+        }
+
+
+        /// <summary>
+        /// UnityEditorから定期的に実行される処理
+        /// </summary>
+        static void RenderingThreadingModeChecker()
+        {
+            //
+            // 外部で変更される可能性がある為、ポーリングして変更を監視して、メニューに反映させています。
+            //
+            if ((PlayerSettings.MTRendering == false) &&
+                (PlayerSettings.graphicsJobs == false))
+            {
+                SetMenu(UnityEngine.Rendering.RenderingThreadingMode.Direct);
+            }
+            else if ((PlayerSettings.MTRendering == true) &&
+                     (PlayerSettings.graphicsJobs == false))
+            {
+                SetMenu(UnityEngine.Rendering.RenderingThreadingMode.MultiThreaded);
+            }
+            else if ((PlayerSettings.MTRendering == true) &&
+                     (PlayerSettings.graphicsJobs == true))
+            {
+                if (PlayerSettings.graphicsJobMode == GraphicsJobMode.Legacy)
+                {
+                    SetMenu(UnityEngine.Rendering.RenderingThreadingMode.LegacyJobified);
+                }
+                else
+                {
+                    SetMenu(UnityEngine.Rendering.RenderingThreadingMode.NativeGraphicsJobs);
+                }
+            }
+            else
+            {
+                SetMenu(UnityEngine.Rendering.RenderingThreadingMode.NativeGraphicsJobsWithoutRenderThread);
+            }
+        }
+
+
+        /// <summary>
+        /// Directが選択された時の処理
+        /// </summary>
+        [MenuItem("Edit/RenderingThreadingMode/Direct", false)]
+        public static void Direct()
+        {
+            PlayerSettings.MTRendering = false;
+            PlayerSettings.graphicsJobs = false;
+        }
+
+
+        /// <summary>
+        /// Directが選択可能であるか否か
+        /// </summary>
+        /// <returns>true:可能 false:不可</returns>
+        [MenuItem("Edit/RenderingThreadingMode/Direct", true)]
+        public static bool IsDirect()
+        {
+            return true;
+        }
+
+
+        /// <summary>
+        /// MultiThreadedが選択された時の処理
+        /// </summary>
+        [MenuItem("Edit/RenderingThreadingMode/MultiThreaded", false)]
+        public static void MultiThreaded()
+        {
+            PlayerSettings.MTRendering = true;
+            PlayerSettings.graphicsJobs = false;
+        }
+
+
+        /// <summary>
+        /// MultiThreadedが選択可能であるか否か
+        /// </summary>
+        /// <returns>true:可能 false:不可能</returns>
+        [MenuItem("Edit/RenderingThreadingMode/MultiThreaded", true)]
+        public static bool IsThreaded()
+        {
+            return true;
+        }
+
+
+        /// <summary>
+        /// LegacyJobifieldが選択された時の処理
+        /// </summary>
+        [MenuItem("Edit/RenderingThreadingMode/LegacyJobified", false)]
+        public static void LegacyJobifield()
+        {
+            PlayerSettings.MTRendering = true;
+            PlayerSettings.graphicsJobs = true;
+            PlayerSettings.graphicsJobMode = GraphicsJobMode.Legacy;
+        }
+
+
+        /// <summary>
+        /// LegacyJobifiedが選択可能であるか否か
+        /// </summary>
+        /// <returns></returns>
+        [MenuItem("Edit/RenderingThreadingMode/LegacyJobified", true)]
+        public static bool IsLegacyJobifield()
+        {
+            return true;
+        }
+
+
+        /// <summary>
+        /// NativeGraphicsJobsが選択された時の処理
+        /// </summary>
+        [MenuItem("Edit/RenderingThreadingMode/NativeGraphicsJobs", false)]
+        public static void NativeGraphicsJobs()
+        {
+            PlayerSettings.MTRendering = true;
+            PlayerSettings.graphicsJobs = true;
+            PlayerSettings.graphicsJobMode = GraphicsJobMode.Native;
+        }
+
+
+        /// <summary>
+        /// NativeGraphicsJobsが選択であるか否か
+        /// </summary>
+        /// <returns></returns>
+        [MenuItem("Edit/RenderingThreadingMode/NativeGraphicsJobs", true)]
+        public static bool IsNativeGraphicsJobs()
+        {
+            return true;
+        }
+
+
+        /// <summary>
+        /// NativeGraphicsJobsWithoutRenderThreadが選択された時の処理
+        /// </summary>
+        [MenuItem("Edit/RenderingThreadingMode/NativeGraphicsJobsWithoutRenderThread", false)]
+        public static void NativeGraphicsJobsWithoutRenderThread()
+        {
+            PlayerSettings.MTRendering = false;
+            PlayerSettings.graphicsJobs = true;
+        }
+
+
+        /// <summary>
+        /// NativeGraphicsJobsWithoutRenderThreadが選択可能であるか否か
+        /// </summary>
+        /// <returns>true:可能 false:不可能</returns>
+        [MenuItem("Edit/RenderingThreadingMode/NativeGraphicsJobsWithoutRenderThread", true)]
+        public static bool IsNativeGraphicsJobsWithoutRenderThread()
+        {
+            return true;
+        }
+    }
+}
+#endif
